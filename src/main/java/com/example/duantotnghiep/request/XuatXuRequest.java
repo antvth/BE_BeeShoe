17c6fe0b306e6eb20ebca56c3d4b244f61129d70
Datptph28141 <Datptph28141@fpt.edu.vn>
2024-08-01 00:55:30 +0700

    update xuatxu

---
+++ src/main/java/com/example/duantotnghiep/request/XuatXuRequest.java
@@ -0,0 +1,24 @@
+package com.example.duantotnghiep.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class XuatXuRequest {
+
+    @NotBlank(message = "Tên xuất xứ không được để trống")
+    private String tenXuatXu;
+
+    @NotNull(message = "Vui lòng chọn trạng thái")
+    private Integer trangThai;
+
+}
